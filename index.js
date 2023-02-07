6df1dd51ac12523a5c9efe394701774674ed57ab
Nicole <106133672+rvnicole@users.noreply.github.com>
2023-02-07 21:33:01 +0000

    Respuesta con JSON  de los datos del Archivo

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const fileUpload = require("express-fileupload");
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
-
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
+});
+
+app.use(fileUpload());
+
+app.post("/api/fileanalyse", (req, res) => {
+  console.log('Hola', req.files.upfile);
+  const { name, size, mimetype } = req.files.upfile;
+  const datos = { name, type: mimetype, size };
+  res.json(datos);
 });
